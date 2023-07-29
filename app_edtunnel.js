69cfbee79c10d017343ae3cfd6129cd1c414d761
boboarts <boboarts@gmail.com>
2023-07-30 02:20:49 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'cfbf79e7-8c9c-4f17-b982-8302d3cd5b79';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
